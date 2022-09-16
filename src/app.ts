694598f03491191dcdc8b46734dd06aba25a779f
Luiz Guilherme <guilhermerx18@gmail.com>
2022-09-16 12:33:33 -0300

    feat: criando servidor express

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import cors from 'cors';
+import express from 'express';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+    this.app.use(cors());
+
+    this.app.get('/', (req, res) => res.status(200).json('Online'))
+  }
+
+
+  public start(PORT: string | number): void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`))
+  }
+}
+
+export default App;
+
+
