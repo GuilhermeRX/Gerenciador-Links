5e8c92390367902f34d533aff14a16d21171d195
Luiz Guilherme <guilhermerx18@gmail.com>
2022-09-17 17:13:23 -0300

    fix: add diretório build no .gitignore

--- Back-End/build/errors/TokenNotFound.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class TokenNotFound extends Error {
-    constructor(message) {
-        super(message);
-    }
-}
-exports.default = TokenNotFound;
