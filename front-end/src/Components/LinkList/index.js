4ab6f5688cb68cfc2c97fef917e3f59b4dddbce4
Luiz Guilherme <guilhermerx18@gmail.com>
2022-09-18 18:32:22 -0300

    feat: renderização dos link card vindos do banco

--- front-end/src/Components/LinkList/index.js
+++ front-end/src/Components/LinkList/index.js
@@ -2,29 +2,23 @@ import { useContext, useState } from "react";
 import { AiOutlineEdit } from 'react-icons/ai';
 import { IoTrashOutline } from 'react-icons/io5';
 import AppContext from "../../context/AppContext";
-import { requestData } from "../../services/fetchAPI";
+import { requestData, setToken } from "../../services/fetchAPI";
+import { notifyError } from "../../services/notify";
 import { Link, List, ListContainer } from "./style";
 export default function LinkList() {
   const { fontColors, colors, list, setList } = useContext(AppContext)
 
   const handleList = async () => {
-    const response = await requestData('/link')
+    const { token } = JSON.parse(localStorage.getItem('token'));
+    setToken(token);
+    const response = await requestData('/link');
+
     if (response.error) {
-      return console.log(response.error);
+      return notifyError(`Error: ${response.error}`)
     }
+
     setList(response);
   }
-
-  const link = (
-    <Link fontColors={fontColors} colors={colors}>
-      <span>Site da Globo</span>
-      <p>www.globo.com</p>
-      <div>
-        <AiOutlineEdit />
-        <IoTrashOutline />
-      </div>
-    </Link>
-  )
 
   useState(() => {
     handleList()
@@ -35,7 +29,16 @@ export default function LinkList() {
     <ListContainer fontColors={fontColors} colors={colors}>
       <span>Gerencie seus links.</span>
       <List>
-        {list.map((obj) => link)}
+        {list.map((obj, index) => (
+          <Link fontColors={fontColors} colors={colors} key={index}>
+            <span>Site da Globo</span>
+            <p>www.globo.com</p>
+            <div>
+              <AiOutlineEdit />
+              <IoTrashOutline />
+            </div>
+          </Link>
+        ))}
       </List>
     </ListContainer>
   )
