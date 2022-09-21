4d3f3585ee89efdebef104bb7b9c40421b99069c
Luiz Guilherme <guilhermerx18@gmail.com>
2022-09-20 21:29:37 -0300

    feat: criando mocks para os testes da rota link

---
+++ Back-End/src/tests/mocks/link.ts
@@ -0,0 +1,35 @@
+import ILink from "src/interfaces/ILink";
+
+export const mockLink: ILink = {
+  id: 1,
+  url: "www.flamengo.com",
+  label: "Site Oficial do Flamengo",
+  userId: 2
+}
+
+export const mockLinkCreate: ILink = {
+  url: "www.betrybe.com",
+  label: "Site da Trybe",
+  userId: 2
+}
+
+export const mockLinkCreateInvalid: ILink = {
+  "url": "www.betrybe.com",
+  "label": "Site da Trybe",
+  "userId": 1.2
+}
+
+export const mockLinkArray: ILink[] = [
+  {
+    id: 1,
+    url: "www.flamengo.com",
+    label: "Site Oficial do Flamengo",
+    userId: 1
+  },
+  {
+    id: 2,
+    url: "www.betrybe.com",
+    label: "Site da Trybe",
+    userId: 3
+  }
+]
