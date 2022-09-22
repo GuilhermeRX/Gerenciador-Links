6ce2930007812a8b02b8875ebdedea420fd3cb28
Luiz Guilherme <guilhermerx18@gmail.com>
2022-09-22 13:21:29 -0300

    fix: removendo comentários do swagger e script autogen

--- Back-End/src/routes/login.route.ts
+++ Back-End/src/routes/login.route.ts
@@ -6,19 +6,6 @@ const loginRoute = Router();
 const loginService = new LoginService();
 const loginController = new LoginController(loginService);
 
-loginRoute.post('/login', (req, res) => (
-  /*
-  #swagger.tags = ['Login']
-  #swagger.description = 'Endpoint para se autenticar na plataforma.'
-  #swagger.parameters['obj'] = {
-    in: 'body',
-    schema: { $ref: '#/definitions/Login' }
-  }
-  #swagger.responses[200] = {
-    description: 'Login efetuado com sucesso.',
-    schema: { $ref: '#/definitions/LoginResponse' }
-  } */
-  loginController.authenticator(req, res)
-));
+loginRoute.post('/login', (req, res) => loginController.authenticator(req, res));
 
 export default loginRoute;
--- Back-End/src/routes/user.route.ts
+++ Back-End/src/routes/user.route.ts
@@ -12,78 +12,13 @@ const userRoute = Router();
 const userService = new UserService();
 const userController = new UserController(userService);
 
-userRoute.post('/user', (req, res) => (
-  /* #swagger.tags = ['User']
-    #swagger.description = 'Endpoint para cadastrar um novo usuário.'
-    #swagger.parameters['obj'] = {
-      in: 'body',
-      schema: { $ref: '#/definitions/CreateUser' }
-    }
-    #swagger.responses[201] = {
-      description: 'User criado com sucesso.',
-      schema: { $ref: '#/definitions/LoginResponse' }
-  } */
-  userController.create(req, res)
-));
+userRoute.post('/user', (req, res) => userController.create(req, res));
 
-userRoute.use((req, res, next) => (
-  /*
-  #swagger.tags = ['User']
-  #swagger.parameters['obj'] = {
-    name: "authorization",
-    in: "header",
-    type: "string"
-  }
-  #swagger.security = [{
-    "bearerAuth": []
-  }]
-  */
-  loginController.validationAuthenticator(req, res, next)
-));
-userRoute.get('/user', (req, res) => (
-  /*
-  #swagger.description = 'Endpoint para obter todos os usuários.'
-  #swagger.responses[200] = {
-    description: 'Listagem de usuários obtida com sucesso.',
-    schema: { $ref: '#/definitions/UserArray' }
-  } */
-  userController.read(req, res)
-));
+userRoute.use((req, res, next) => loginController.validationAuthenticator(req, res, next));
 
-userRoute.get('/user/info', (req, res) => (
-  /*
-  #swagger.description = 'Endpoint para obter um usuário especifico através de dados obtidos via token jwt.'
-  #swagger.responses[200] = {
-    description: 'Usuário obtido com sucesso.',
-    schema: { $ref: '#/definitions/User' }
-  } */
-  userController.readOne(req, res)
-));
-
-userRoute.put('/user/:id', (req, res) => (
-  /*
-  #swagger.description = 'Endpoint para atualizar os dados de um usuário.'
-  #swagger.parameters['id'] = { description: 'ID do usuário.' }
-  #swagger.parameters['obj'] = {
-      in: 'body',
-      type: 'object',
-      schema: { $ref: '#/definitions/CreateUser' }
-  }
-  #swagger.responses[200] = {
-    description: 'Usuário editado com sucesso.',
-    schema: { $ref: '#/definitions/User' }
-  } */
-  userController.update(req, res)
-));
-
-userRoute.delete('/user/:id', (req, res) => (
-  /*
-  #swagger.description = 'Endpoint para deletar um usuário.'
-  #swagger.parameters['id'] = { description: 'ID do usuário.' }
-  #swagger.responses[204] = {
-    description: 'Usuário deletado com sucesso.',
-  } */
-  userController.delete(req, res)
-));
+userRoute.get('/user', (req, res) => userController.read(req, res));
+userRoute.get('/user/info', (req, res) => userController.readOne(req, res));
+userRoute.put('/user/:id', (req, res) => userController.update(req, res));
+userRoute.delete('/user/:id', (req, res) => userController.delete(req, res));
 
 export default userRoute;
